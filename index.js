c8ae43fd6a86304ab16c8266254de41acffae742
putla venkat praneeth <putlavenkat123@gmail.com>
2024-11-09 09:33:48 +0530

    9thnov

---
+++ index.js
@@ -0,0 +1,8 @@
+function arr(a,b,c,d){
+
+console.log(a,b,c,d,);
+
+
+
+}
+arr([1,2,3,4]);
---
+++ js1.js
@@ -0,0 +1,12 @@
+// var x=()=>{return "hello"};
+// console.log(x());
+
+var x=()=>"hello";
+console.log(x());
+
+
+//let=no declaration//re-assign//no-re initialization;
+//var= declaration/re-assign//re initialization;
+//var has no block scope//access out of block also//has function scope
+//let has block scope//cannot access out of block//has function scope
+//const has block scope//has function scope
