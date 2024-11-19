8e5b3d7b264f8ff425024287944fb4389c845b52
putla venkat praneeth <putlavenkat123@gmail.com>
2024-11-19 21:08:10 +0530

    arrays

---
+++ array.js
@@ -0,0 +1,11 @@
+let arr = [1, 2, 3, 4, 5];
+let x = arr.map((a) => {
+  return a * a;
+});
+console.log(x);
+
+let arr1 = ["rin", "pin", "tin"];
+let x1 = arr1.map((a) => {
+  return a.toUpperCase();
+});
+console.log(x1);
